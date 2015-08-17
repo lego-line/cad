cb007aee45a441c6e1c465ad7cb00834e5f00c9a
Eric Wieser <wieser.eric@gmail.com>
2015-08-17 18:17:10 +0100

    Update snapping script to actually work

--- snap_to_grid.py
+++ snap_to_grid.py
@@ -5,13 +5,25 @@ for line in fileinput.input(inplace=True):
     line = line.split(' ')
     new_line = []
     
-    for cell in line:
-        try:
-            new_cell = Decimal(cell)
-        except InvalidOperation:
-            new_cell = cell
+    for i, cell in enumerate(line):
+        if i in range(2, 5):
+            step = Decimal('5')
+        elif i in range(5, 14):
+            step = Decimal('0.001')
         else:
-            new_cell = str(new_cell.quantize(Decimal('5')))
+            step = None
+
+        new_cell = cell
+
+        if step:
+            try:
+                new_cell = Decimal(cell)
+            except InvalidOperation:
+                pass
+            else:
+                new_cell = (new_cell / step).quantize(1) * step
+                new_cell = str(new_cell)
+
         new_line.append(new_cell)
         
     print ' '.join(new_line),
